65d5b0f8982bbc65b2c3e585994ec539462f9c51
thafak <marco_estela_ferreira@hotmail.com>
2025-04-15 11:33:19 +0100

    [Two Pointers][Medium] Solved #15 3Sum

---
+++ Two-pointers/medium/2-3sum.js
@@ -0,0 +1,42 @@
+// Problem: 3Sum
+
+// Category: Two Pointers
+
+// Difficulty: Medium
+
+// Description: Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
+// such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+// Notice that the solution set must not contain duplicate triplets.
+
+var threeSum = function(nums) {
+    const solution = [];
+
+    nums.sort((a, b) => a - b);
+
+    let target = 0;
+    for (let i = 0; i < nums.length - 2; i++) {
+        // Skip duplicates for the current element
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let j = i + 1;
+        let k = nums.length - 1;
+        let partialTarget = target - nums[i];
+
+        while (j < k) {
+            let partialSum = nums[j] + nums[k];
+            if (partialSum === partialTarget) {
+                solution.push([nums[i], nums[j], nums[k]]);
+                // Skip duplicates for the second and third elements
+                while (j < k && nums[j] === nums[j + 1]) j++;
+                while (j < k && nums[k] === nums[k - 1]) k--;
+                j++;
+                k--;
+            } else if (partialSum > partialTarget) {
+                k--;
+            } else {
+                j++;
+            }
+        }
+    }
+    return solution;
+};
